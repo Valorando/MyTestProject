ab0e68d9cd8301f1cee43896be68d80d7479c0d6
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2024-11-09 11:13:16 +0200

    added new controller

---
+++ MyTestProjectAPI/MyTestProjectAPI/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("hello")]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
